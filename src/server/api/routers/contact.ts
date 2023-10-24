18d591b18ebe9880272b445d401480b2f1a739f9
magnus <magnus@dahleide.com>
2023-10-24 20:37:16 +0200

    Remove unused ctx variable

--- src/server/api/routers/contact.ts
+++ src/server/api/routers/contact.ts
@@ -8,19 +8,17 @@ import {
 import { db } from "~/server/db";
 
 export const contactRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(contactSchema)
-    .mutation(async ({ input, ctx }) => {
-      const msg = await db.contactMessage.create({
-        data: {
-          name: input.name,
-          email: input.email,
-          subject: input.subject,
-          message: input.message,
-        },
-      });
-      return { success: true, msg };
-    }),
+  hello: publicProcedure.input(contactSchema).mutation(async ({ input }) => {
+    const msg = await db.contactMessage.create({
+      data: {
+        name: input.name,
+        email: input.email,
+        subject: input.subject,
+        message: input.message,
+      },
+    });
+    return { success: true, msg };
+  }),
 
   // getAll: publicProcedure.query(({ ctx }) => {
   //   return ctx.db.example.findMany();
