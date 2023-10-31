c5a7535622120168f57859c7b6ebab3c18410572
magnus <magnus@dahleide.com>
2023-10-31 19:13:48 +0100

    Add SST to project

--- next.config.mjs
+++ next.config.mjs
@@ -18,16 +18,6 @@ const config = {
       },
     ],
   },
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   experimental: {
     swcPlugins: [
       [
---
+++ sst.config.ts
@@ -0,0 +1,20 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "newsite",
+      region: "eu-north-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site");
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
