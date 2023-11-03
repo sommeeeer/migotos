6a9db855048e8b669c239324d7fc35725595bf5a
magnus <magnus@dahleide.com>
2023-11-03 14:51:17 +0100

    Add upload test route

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -7,21 +7,19 @@ export default function Home({ url }: { url: string }) {
   return (
     <main>
       <form
-        onSubmit={async (e) => {
+        onSubmit={(e) => {
           e.preventDefault();
 
-          const file = (e.target as HTMLFormElement).file.files?.[0]!;
+          const target = e.target as typeof e.target & {
+            file: { files: FileList };
+          };
+          const file = target.file.files[0];
 
-          const image = await fetch(url, {
-            body: file,
-            method: "PUT",
-            headers: {
-              "Content-Type": file.type,
-              "Content-Disposition": `attachment; filename="${file.name}"`,
-            },
-          });
-          console.log(image.url.split("?")[0]!);
-          window.location.href = image.url.split("?")[0]!;
+          if (file) {
+            console.log(file);
+          } else {
+            console.log("no file selected");
+          }
         }}
       >
         <input name="file" type="file" accept="image/png, image/jpeg" />
