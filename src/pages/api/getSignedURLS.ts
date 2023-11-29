d056dc23853624a07d998d8137da7488b2285579
magnus <magnus@dahleide.com>
2023-11-29 07:36:57 +0100

    Add route to get signed urls

---
+++ src/pages/api/getSignedURLS.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerAuthSession } from "~/server/auth";
+import { getSignedURLS } from "~/server/helpers";
+
+type ResponseData = {
+  message?: string;
+  urls?: string[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
+) {
+  if (req.method !== "GET") {
+    res.status(405).json({ message: "Method not allowed" });
+    return;
+  }
+  const session = await getServerAuthSession({
+    req,
+    res,
+  });
+  if (!session) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+
+  const { amount } = req.query;
+  if (!amount || isNaN(Number(amount))) {
+    res.status(400).json({ message: "Amount must be a valid number" });
+    return;
+  }
+
+  const urls = await getSignedURLS(Number(amount));
+
+  if (!urls) {
+    res.status(500).json({ message: "Internal server error" });
+    return;
+  }
+
+  res.status(200).json({ urls: urls });
+}
