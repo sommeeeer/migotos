fb5763e4878b3716fc733e6f4637f81e3e1b94fc
magnus <magnus@dahleide.com>
2023-11-29 13:07:04 +0100

    Add signIn to login button for comments

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,11 @@
-import { popupCenter } from "~/utils/helpers";
+import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
   return (
     <div className="flex flex-col gap-4">
       <h3 className="text-base">Want to post a comment?</h3>
       <button
-        onClick={() => popupCenter("/google-signin", "Google Sign In")}
+        onClick={() => signIn()}
         className="h-14 w-4/6 cursor-pointer rounded-md border-2 border-solid border-gray-200 p-4 text-base transition-all duration-300 ease-in-out hover:bg-hoverbg hover:text-white disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-200 disabled:text-gray-600"
         type="submit"
       >
