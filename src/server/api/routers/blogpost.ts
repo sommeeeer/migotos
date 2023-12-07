02445f9418e2e322070050e83ced651eeb7e9555
magnus <magnus@dahleide.com>
2023-12-07 09:53:39 +0100

    Update blogpost procedure to revalidate path

--- src/server/api/routers/blogpost.ts
+++ src/server/api/routers/blogpost.ts
@@ -8,63 +8,91 @@ import { db } from "~/server/db";
 export const blogpostRouter = createTRPCRouter({
   createBlogPost: protectedProcedure
     .input(blogPostSchema)
-    .mutation(async ({ input }) => {
-      const blogpost = await db.blogPost.create({
-        data: {
-          title: input.title,
-          body: input.body,
-          post_date: input.post_date,
-          image_url: input.image_url,
-        },
-      });
-      return blogpost;
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const blogpost = await db.blogPost.create({
+          data: {
+            title: input.title,
+            body: input.body,
+            post_date: input.post_date,
+            image_url: input.image_url,
+          },
+        });
+        await ctx.res.revalidate("/news/");
+        await ctx.res.revalidate("/");
+        return blogpost;
+      } catch (err) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
+        });
+      }
     }),
   deleteBlogPost: protectedProcedure
     .input(z.number())
-    .mutation(async ({ input }) => {
-      const blogpost = await db.blogPost.findFirst({
-        where: {
-          id: input,
-        },
-      });
-      if (!blogpost) {
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const blogpost = await db.blogPost.findFirst({
+          where: {
+            id: input,
+          },
+        });
+        if (!blogpost) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Blogpost not found",
+          });
+        }
+        const deletedBlogPost = await db.blogPost.delete({
+          where: {
+            id: input,
+          },
+        });
+        await ctx.res.revalidate("/news/");
+        await ctx.res.revalidate("/");
+        return deletedBlogPost;
+      } catch (err) {
         throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Blogpost not found",
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
         });
       }
-      const deletedBlogPost = await db.blogPost.delete({
-        where: {
-          id: input,
-        },
-      });
-      return deletedBlogPost;
     }),
   updateBlogPost: protectedProcedure
     .input(blogPostSchema.extend({ id: z.number() }))
-    .mutation(async ({ input }) => {
-      const blogpost = await db.blogPost.findUnique({
-        where: {
-          id: input.id,
-        },
-      });
-      if (!blogpost) {
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const blogpost = await db.blogPost.findUnique({
+          where: {
+            id: input.id,
+          },
+        });
+        if (!blogpost) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Blogpost not found",
+          });
+        }
+        const updatedBlogPost = await db.blogPost.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            title: input.title,
+            body: input.body,
+            post_date: input.post_date,
+            image_url: input.image_url,
+          },
+        });
+        await ctx.res.revalidate("/news/");
+        await ctx.res.revalidate("/");
+        await ctx.res.revalidate(`/news/${input.id}}`);
+        return updatedBlogPost;
+      } catch (err) {
         throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Blogpost not found",
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
         });
       }
-      const updatedBlogPost = await db.blogPost.update({
-        where: {
-          id: input.id,
-        },
-        data: {
-          title: input.title,
-          body: input.body,
-          post_date: input.post_date,
-          image_url: input.image_url,
-        },
-      });
-      return updatedBlogPost;
     }),
 });
