e1f851faf7676a9bac084023988e5a9481c90a97
magnus <magnus@dahleide.com>
2024-01-02 07:40:55 +0100

    Add cdk.context.json and update customDomain in sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,5 @@
 import type { SSTConfig } from "sst";
 import { Bucket, NextjsSite } from "sst/constructs";
-import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 export default {
   config(_input) {
@@ -13,17 +12,7 @@ export default {
     app.stack(function Site({ stack }) {
       const bucket = new Bucket(stack, "public");
       const site = new NextjsSite(stack, "site", {
-        // customDomain: {
-        //   domainName: "next.migotos.com",
-        //   isExternalDomain: true,
-        //   cdk: {
-        //     certificate: Certificate.fromCertificateArn(
-        //       stack,
-        //       "MyCert",
-        //       "arn:awsblabla",
-        //     ),
-        //   },
-        // },
+        customDomain: "migotos.com",
         bind: [bucket],
       });
 
