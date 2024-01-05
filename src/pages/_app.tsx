6d840daa07435a28ff6f7edd988985c0976f0e80
magnus <magnus@dahleide.com>
2024-01-05 22:19:01 +0100

    Add all icons in _app.tsx and delete unused ones

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,22 +30,78 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <Head>
         <link
           rel="icon"
-          href="https://www.migotos.com/wp-content/uploads/2017/03/cropped-socialicon-1-32x32.png"
+          href="/static/icons/cropped-socialicon-32x32.png"
           sizes="32x32"
         />
         <link
           rel="icon"
-          href="https://www.migotos.com/wp-content/uploads/2017/03/cropped-socialicon-1-192x192.png"
+          href="/static/icons/cropped-socialicon-192x192.png"
           sizes="192x192"
         />
         <link
           rel="apple-touch-icon"
-          href="https://www.migotos.com/wp-content/uploads/2017/03/cropped-socialicon-1-180x180.png"
+          href="/static/icons/cropped-socialicon-180x180.png"
         />
         <meta
           name="msapplication-TileImage"
-          content="https://www.migotos.com/wp-content/uploads/2017/03/cropped-socialicon-1-270x270.png"
+          content="/static/icons/cropped-socialicon-270x270.png"
         />
+
+        <link
+          rel="icon"
+          href="/static/icons/browser-150x150.png"
+          sizes="150x150"
+        />
+        <link
+          rel="icon"
+          href="/static/icons/browser-300x300.png"
+          sizes="300x300"
+        />
+        <link
+          rel="icon"
+          href="/static/icons/browser-480x480.png"
+          sizes="480x480"
+        />
+        <link rel="icon" href="/static/icons/browser.png" />
+
+        <link
+          rel="icon"
+          href="/static/icons/cropped-socialicon-150x150.png"
+          sizes="150x150"
+        />
+        <link
+          rel="icon"
+          href="/static/icons/cropped-socialicon-480x480.png"
+          sizes="480x480"
+        />
+        <link
+          rel="icon"
+          href="/static/icons/cropped-socialicon-512x400.png"
+          sizes="512x400"
+        />
+        <link rel="icon" href="/static/icons/cropped-socialicon.png" />
+
+        <link
+          rel="icon"
+          href="/static/icons/socialicon-150x150.png"
+          sizes="150x150"
+        />
+        <link
+          rel="icon"
+          href="/static/icons/socialicon-300x300.png"
+          sizes="300x300"
+        />
+        <link
+          rel="icon"
+          href="/static/icons/socialicon-480x480.png"
+          sizes="480x480"
+        />
+        <link
+          rel="icon"
+          href="/static/icons/socialicon-512x400.png"
+          sizes="512x400"
+        />
+        <link rel="icon" href="/static/icons/socialicon.png" />
       </Head>
       <div
         className={`flex flex-col ${poppins.className} ${playfair.variable}`}
