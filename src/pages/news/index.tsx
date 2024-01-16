9289eca9dbe781ed9d334844b12135bea596a0ea
magnus <magnus@dahleide.com>
2024-01-16 12:24:30 +0100

    Add meta tags for SEO in News page

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { type GetStaticPropsResult } from "next/types";
 import Footer from "~/components/Footer";
 import NewsCard from "~/components/ui/NewsCard";
@@ -11,6 +12,42 @@ type Props = {
 function News({ blogPosts }: Props) {
   return (
     <>
+      <Head>
+        <title>Migotos - All Blog Posts</title>
+        <meta
+          name="description"
+          content="All the blogposts for Migotos, Norwegian Forest Cat Cattery based in Oslo, Norway"
+        />
+        <meta property="og:site_name" content="Migotos - All Blog Posts" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content="Migotos - All Blog Posts" />
+        <meta
+          property="og:description"
+          content="All the blogposts for Migotos, Norwegian Forest Cat Cattery based in Oslo, Norway"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://migotos.com/news" />
+        <meta
+          property="og:image"
+          content="https://migotos.com/static/icons/cropped-socialicon-480x480.png"
+        />
+        <meta property="og:image:alt" content="Migotos logo" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="480" />
+        <meta property="og:image:height" content="480" />
+        <meta
+          property="article:published_time"
+          content="2024-01-16T12:18:00+01:00"
+        />
+        <meta
+          property="article:modified_time"
+          content="2024-01-16T12:18:00+01:00"
+        />
+        <meta
+          property="article:author"
+          content="https://www.facebook.com/eva.d.eide"
+        />
+      </Head>
       <div className="flex w-full flex-col items-center gap-8">
         <section className="mt-8 flex max-w-6xl flex-col gap-4 px-4 text-center">
           <h1 className="font-playfair text-4xl">
