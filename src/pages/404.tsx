acfead923b31b5be544bbd157f794340be90aa1d
magnus <magnus@dahleide.com>
2024-01-16 22:51:25 +0100

    Add home button and update page title in 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,36 @@
-import { ArrowBigLeftIcon } from "lucide-react";
+import { ArrowBigLeftIcon, Home } from "lucide-react";
 import { useRouter } from "next/router";
 import { Button } from "~/components/ui/button";
 
 export default function Custom404() {
   const router = useRouter();
   return (
-    <div className="mt-24 flex flex-col items-center gap-4">
+    <div className="mt-16 flex flex-col items-center gap-4">
       <div className="text-center">
-        <h1 className="bold text-8xl text-gray-800">404</h1>
-        <h3 className="bold text-6xl text-gray-800">Not Found</h3>
+        <h1 className="bold text-7xl text-gray-800">404</h1>
+        <h3 className="bold text-5xl text-gray-800">Page Not Found</h3>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
       </div>
-      <Button className="mt-4" variant="outline" onClick={() => router.back()}>
-        <ArrowBigLeftIcon className="mr-2" />
-        Go back
-      </Button>
+      <div className="flex gap-2">
+        <Button
+          className="mt-4"
+          variant="outline"
+          onClick={() => router.push("/")}
+        >
+          <Home className="mr-2" />
+          Go Home
+        </Button>
+        <Button
+          className="mt-4"
+          variant="outline"
+          onClick={() => router.back()}
+        >
+          <ArrowBigLeftIcon className="mr-2" />
+          Go back
+        </Button>
+      </div>
     </div>
   );
 }
